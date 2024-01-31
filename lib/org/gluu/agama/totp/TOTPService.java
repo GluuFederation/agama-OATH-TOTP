80e741006de260e669951f2c04ed2c60e3b9df16
kdhttps <kdhttps@gmail.com>
2024-01-31 13:53:57 +0530

    feat: update TOTP service

--- lib/org/gluu/agama/totp/TOTPService.java
+++ lib/org/gluu/agama/totp/TOTPService.java
@@ -7,9 +7,9 @@ public abstract class TOTPService {
 
     public abstract String generateSecretKey(int keyLength);
     public abstract String generateTotpSecretKeyUri(String totpSecretKey, String issuer, String userDisplayName);
-    public boolean validateTOTP(String clientTOTP, String totpSecretKey, String alg);
-    public String linkUser(String uid, String totpSecretKey);
-    public String getUserTOTPSecretKey(String uid);
+    public abstract boolean validateTOTP(String clientTOTP, String totpSecretKey, String alg);
+    public abstract String linkUser(String uid, String totpSecretKey);
+    public abstract String getUserTOTPSecretKey(String uid);
 
     public static TOTPService getInstance(){
         return  JansTOTPService.getInstance();
