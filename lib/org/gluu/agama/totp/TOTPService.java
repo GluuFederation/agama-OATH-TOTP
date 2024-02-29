86a75503a02d0d5e320b88d338fc383f737fd3eb
kdhttps <kdhttps@gmail.com>
2024-02-29 14:56:47 +0530

    feat: add nickname

--- lib/org/gluu/agama/totp/TOTPService.java
+++ lib/org/gluu/agama/totp/TOTPService.java
@@ -7,7 +7,7 @@ public abstract class TOTPService {
     public abstract String generateSecretKey(int keyLength);
     public abstract String generateTotpSecretKeyUri(String totpSecretKey, String issuer, String userDisplayName);
     public abstract boolean validateTOTP(String clientTOTP, String totpSecretKey, String alg);
-    public abstract String linkUser(String uid, String totpSecretKey);
+    public abstract String linkUser(String uid, String totpSecretKey, String nickName);
     public abstract String getUserTOTPSecretKey(String uid);
 
     public static TOTPService getInstance(){
